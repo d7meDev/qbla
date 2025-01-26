89a33fb6b3e73108945d974bda9b91c1dc7f23d5
d7meDev <d7me446@gmail.com>
2025-01-26 22:37:33 +0300

    a

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const degree = document.getElementById("degree");
 const img = document.querySelector('img');
-
 const fire = () => { window.addEventListener('deviceorientation' ,event => {
     
     setTimeout(() => {
         degree.textContent = `${event.alpha.toFixed(0)} deg`;
-        img.style.transform = `rotate(${event.alpha.toFixed(0)}deg)`
+        img.style.transform = `translate(-50%,-50%) rotate(${event.alpha.toFixed(0)}deg)`
     },500)
 })};
 
