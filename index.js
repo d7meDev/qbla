a3de0fccfd9aaf58b3f4ce6a41846543f5dbc0d4
d7meDev <d7me446@gmail.com>
2025-01-26 21:06:47 +0300

    x

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const fire = () => { window.addEventListener('deviceorientation' ,event => {
     degree.textContent = `${event.alpha} deg`;
 })};
 
-if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-    DeviceOrientationEvent.requestPermission().then((permissionState) => {
-        if (permissionState === 'granted') {
-            fire();
-        } else {
-            console.log('Permission denied for device orientation.');
-        }
-    });
-}
-else{
-    fire();
-}
 
 
+async function compass(){
+
+    if ( typeof DeviceOrientationEvent != undefined && typeof DeviceOrientationEvent.requestPermission === 'function') {
+        
+        const permissionState = await DeviceOrientationEvent.requestPermission();
+            if (permissionState === 'granted') {
+                fire();
+               
+            } else {
+                alert('Permission denied for device orientation');
+            }
+      
+    }
+    else if('DeviceOrientationEvent' in window){
+        fire();
+        
+    }
+    else{
+        alert("This device dose not support device orientation")
+    }
+}
 
 
